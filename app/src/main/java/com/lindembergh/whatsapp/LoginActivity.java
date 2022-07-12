2d7e267fcf7d51935c307d151977c2c838356122
Lindembergh Madruga <lindemberghferreira@gmail.com>
2022-07-12 10:33:59 -0300

    Criando interface de cadastro

--- app/src/main/java/com/lindembergh/whatsapp/LoginActivity.java
+++ app/src/main/java/com/lindembergh/whatsapp/LoginActivity.java
@@ -1,8 +1,9 @@
 package com.lindembergh.whatsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void abrirTelaCadastro(View view){
+        Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
+        startActivity(intent);
     }
 }
